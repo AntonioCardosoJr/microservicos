9882e53c555ffbdd0d54885802d54fd2ff227ad4
Antonio <t1006012@marfrig.com.br>
2021-12-01 11:08:09 -0300

    RestTemplate

---
+++ hr-payroll/src/main/java/br/com/tecnocontrol/hrpayroll/config/AppConfig.java
@@ -0,0 +1,14 @@
+package br.com.tecnocontrol.hrpayroll.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+	@Bean
+	public RestTemplate restTeplate() {
+		return new RestTemplate();
+	}
+}
