436033f8018514ba0b890ebca71d3aa080adfb4e
Antonio Geraldo Cardoso Junior <>
2022-01-09 13:42:07 -0300

    Project hr-user created

---
+++ hr-user/src/test/java/br/com/tcagro/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tcagro.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
